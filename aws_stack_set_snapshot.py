34d3c3f7898be4277eb9b405250bf8ab6243ecce
Iain Samuel McLean Elder <iain@isme.es>
2022-10-31 13:10:44 +0100

    Replace collator with typed paginators

--- aws_stack_set_snapshot.py
+++ aws_stack_set_snapshot.py
@@ -1,22 +1,33 @@
 import boto3
 import botocore.config
-from boto_collator_client import CollatorClient  # type: ignore[import]
 import json
 import sys
 import concurrent.futures as fut
 import logging
 import datetime
-from typing import Any
+from typing import Any, Dict, Literal, List
+from mypy_boto3_cloudformation import CloudFormationClient
+from mypy_boto3_cloudformation.type_defs import (
+    StackSetSummaryTypeDef,
+    StackInstanceSummaryTypeDef,
+    StackSetOperationSummaryTypeDef,
+    StackSetTypeDef,
+)
 
 # TODO make this work for logdecorator
 # logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
+class StackSet(StackSetTypeDef):
+    Instances: List[StackInstanceSummaryTypeDef]
+    Operations: List[StackSetOperationSummaryTypeDef]
+
+
 def main() -> None:
     session = boto3.Session()
     cfn = configure_client(session)
     
-    stack_sets = cfn.list_stack_sets()["Summaries"]
+    stack_sets: List[StackSetSummaryTypeDef] = cfn.list_stack_sets()["Summaries"]
 
     with fut.ThreadPoolExecutor(max_workers=2) as executor:
         future_to_stack_set = {
@@ -27,12 +38,12 @@ def main() -> None:
     
     for f in fut.as_completed(future_to_stack_set):
         stack_set = future_to_stack_set[f]
-        stack_set.update(f.result())
+        stack_set.update(f.result())  # type: ignore[typeddict-item]
     
     dump_snapshot_to_json(stack_sets)
 
 
-def configure_client(session: boto3.Session) -> CollatorClient:
+def configure_client(session: boto3.Session) -> CloudFormationClient:
 
     class RetryFilter(logging.Filter):
 
@@ -51,23 +62,25 @@ def configure_client(session: boto3.Session) -> CollatorClient:
         }
     )
 
-    return CollatorClient(session.client("cloudformation", config=config))
+    return session.client("cloudformation", config=config)
 
 # @ld.log_on_start(
 #     logging.INFO,
 #     message="""Instances for stack set {stack_set["StackSetName"]}""",
 #     logger=logger)
-def list_instances(cfn: CollatorClient, stack_set: Any) -> Any:
-    instances = cfn.list_stack_instances(StackSetName=stack_set["StackSetId"])["Summaries"]
+def list_instances(cfn: CloudFormationClient, stack_set: StackSetSummaryTypeDef) -> Dict[Literal["Instances"], List[StackInstanceSummaryTypeDef]]:
+    pages = cfn.get_paginator("list_stack_instances").paginate(StackSetName=stack_set["StackSetId"])
+    instances = [inst for page in pages for inst in page["Summaries"]]
     return {"Instances": instances}
 
 
-def list_operations(cfn: CollatorClient, stack_set: Any) -> Any:
-    operations = cfn.list_stack_set_operations(StackSetName=stack_set["StackSetId"])["Summaries"]
+def list_operations(cfn: CloudFormationClient, stack_set: StackSetSummaryTypeDef) -> Dict[Literal["Operations"], List[StackSetOperationSummaryTypeDef]]:
+    pages = cfn.get_paginator("list_stack_set_operations").paginate(StackSetName=stack_set["StackSetId"])
+    operations = [op for page in pages for op in page["Summaries"]]
     return {"Operations": operations}
 
 
-def describe_stack_set(cfn: CollatorClient, stack_set: Any) -> Any:
+def describe_stack_set(cfn: CloudFormationClient, stack_set: StackSetSummaryTypeDef) -> StackSetTypeDef:
     description = cfn.describe_stack_set(StackSetName=stack_set["StackSetId"])["StackSet"]
     return description
 
